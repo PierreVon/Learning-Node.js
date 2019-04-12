47dda8c6ab31bbd66c76943dadc120d365f9a8ba
PierreVon <pierrevon@PierreVondeMacBook-Air.local>
2019-04-12 21:28:14 +0800

    router middleware

--- 1-router/routes/index.js
+++ 1-router/routes/index.js
@@ -4,6 +4,15 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  next();
+});
+
+router.get('/', (req, res, next) => {
+  console.log('I can be printed')
+});
+
+router.get('/', (req, res, next) => {
+    console.log("I can't be printed")
 });
 
 module.exports = router;

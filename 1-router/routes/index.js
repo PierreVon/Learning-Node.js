ccd3412de635fb92d8d8a601edb4ee7a4f993192
PierreVon <pierrevon@PierreVondeMacBook-Air.local>
2019-04-13 09:49:48 +0800

    combination of middleware

--- 1-router/routes/index.js
+++ 1-router/routes/index.js
@@ -5,14 +5,39 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
   next();
-});
+})
 
-router.get('/', (req, res, next) => {
-  console.log('I can be printed')
-});
+    // Execute any code.
+    // Make changes to the request and the response objects.
+    // End the request-response cycle.
+    // Call the next middleware in the stack.
 
-router.get('/', (req, res, next) => {
+// combination of middleware
+var cb0 = function (req, res, next) {
+    console.log('CB0')
+    next()
+}
+
+var cb1 = function (req, res, next) {
+    console.log('CB1')
+    next()
+}
+
+router.get('/', [cb0, cb1], function (req, res, next) {
+    console.log('I can be printed')
+}, function (req, res) {
     console.log("I can't be printed")
-});
+})
+
+// router.route('/book')
+//     .get(function (req, res) {
+//         res.send('Get a random book')
+//     })
+//     .post(function (req, res) {
+//         res.send('Add a book')
+//     })
+//     .put(function (req, res) {
+//         res.send('Update the book')
+//     })
 
 module.exports = router;
--- 1-router/routes/users.js
+++ 1-router/routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// Route path: /user/:userId(\d+)
+// Request URL: http://localhost:3000/user/42
+// req.params: {"userId": "42"}
+
 module.exports = router;
